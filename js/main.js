51cddb5b94dccaf344ea0cfdb2c308ee526b3f57
praneetnadella <praneetnadella@gmail.com>
2019-08-13 15:20:32 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
 	"use strict";
 	var nav = $('nav');
-  	var navHeight = nav.outerHeight();
+	var navHeight = nav.outerHeight();
   
 	$('.navbar-toggler').on('click', function() {
 		if( ! $('#mainNav').hasClass('navbar-reduce')) {
